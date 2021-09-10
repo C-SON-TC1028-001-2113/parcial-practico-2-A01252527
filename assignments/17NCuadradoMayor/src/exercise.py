998075b10f1e53f26bc540393826237b8851979d
A01252527 <a01252527@itesm.mx>
2021-09-10 17:29:20 +0000

    entrega2

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,13 @@
 
-
 def main():
-    numero = int(input("Escribe un numero : "))
+    
     #escribe tu código abajo de esta línea
+    numero = int(input("Escribe un numero : "))
+    answer = 1
 
-
+    while answer**2<=numero:
+        answer += 1
+    
+    print(answer)
 if __name__=='__main__':
     main()
